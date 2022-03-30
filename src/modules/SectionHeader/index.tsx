0c030d56e6778d6f425a9f4cb79d6323bd3f6229
Thiago Almeida <thiago.almeida@solides.com.br>
2022-03-29 23:04:30 -0300

    add folder module and section header

---
+++ src/modules/SectionHeader/index.tsx
@@ -0,0 +1,12 @@
+import Typography from "components/Typography";
+
+const SectionHeader = () => {
+
+  return (
+    <>
+      <Typography type="link" href="asd">asd</Typography>
+    </>
+  )
+}
+
+export default SectionHeader;
