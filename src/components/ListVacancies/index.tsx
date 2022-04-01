bb63db07de77188856eee9ec42c974a67c524f3f
Thiago Almeida <thiago.almeida@solides.com.br>
2022-04-01 13:42:18 -0300

    tratment get list vacancies

--- src/components/ListVacancies/index.tsx
+++ src/components/ListVacancies/index.tsx
@@ -5,10 +5,42 @@ type ListVacanciesProps = {
   vacanciesList?: any
 }
 
+type LocationVacancieProps = {
+  bairro: string
+  cidade: string
+  pais: string
+}
+
+type VacancieProps = {
+  ativa: boolean
+  cargo: string
+  link: string
+  localizacao?: LocationVacancieProps
+}
+
 const ListVacancies = ({ vacanciesList }: ListVacanciesProps) => {
+  const vacanciesListTratment = () => {
+    return vacanciesList?.vagas.filter((item: VacancieProps) => {
+      return item.ativa
+    })
+  }
+  const location = (value: LocationVacancieProps | undefined | null) => {
+    return value ? `${value.bairro} - ${value.cidade}` : 'Remoto'
+  }
+
   return (
     <StyledContainer>
       <Typography type="subtitle">Desenvolvimento</Typography>
+      <table>
+        {vacanciesListTratment().map((item: VacancieProps, index: number) => (
+          <tr key={index}>
+            <a href={item.link} target="blank">
+              <td>{item.cargo}</td>
+              <td>{location(item.localizacao)}</td>
+            </a>
+          </tr>
+        ))}
+      </table>
     </StyledContainer>
   )
 }
